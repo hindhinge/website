d246b2a0da8b2bab0a0c97aee9b10395ccfebe46
Radosław Sawicki <44055884+hindhinge@users.noreply.github.com>
2020-07-13 10:30:37 +0200

    Updates

---
+++ precache-manifest.bf5d7c04d9ff63f3b5d1ad85a72707ed.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b34ea7886642bff3b19ccac616a6221",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "290d4c5ac9e69963c764",
+    "url": "/website/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a61b797724c8686a15b3",
+    "url": "/website/static/css/main.2f3e2a12.chunk.css"
+  },
+  {
+    "revision": "290d4c5ac9e69963c764",
+    "url": "/website/static/js/2.1a80b252.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/website/static/js/2.1a80b252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61b797724c8686a15b3",
+    "url": "/website/static/js/main.0d800ac4.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "2c724060e1e710f3fd169a7aa27ec69b",
+    "url": "/website/static/media/blackjack_1.2c724060.png"
+  },
+  {
+    "revision": "f93a4ca79614a99d346465cc56a2b8f0",
+    "url": "/website/static/media/blackjack_2.f93a4ca7.png"
+  },
+  {
+    "revision": "c5704f0c0deb0f56e8710b685df96e19",
+    "url": "/website/static/media/browar_1.c5704f0c.png"
+  },
+  {
+    "revision": "64f362d13020f3aed36d5380e23f4b85",
+    "url": "/website/static/media/browar_2.64f362d1.png"
+  },
+  {
+    "revision": "e87c734b4717a09a2bf22e0cea1c82e3",
+    "url": "/website/static/media/down_arrow.e87c734b.png"
+  },
+  {
+    "revision": "5622613a465f56e212cb16a918f90f1f",
+    "url": "/website/static/media/ftp_1.5622613a.png"
+  },
+  {
+    "revision": "e46908e214587e21367f484cad9bbe48",
+    "url": "/website/static/media/ftp_2.e46908e2.png"
+  },
+  {
+    "revision": "b41c3d696b8150fb51340a513fa797c6",
+    "url": "/website/static/media/ftp_3.b41c3d69.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66afcc4b88245ec55dece4efe0395b89",
+    "url": "/website/static/media/logo_Asp.66afcc4b.png"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/website/static/media/logo_Cpp.0b849c72.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/website/static/media/logo_Github.3e54ed15.png"
+  },
+  {
+    "revision": "fb997f5b614228fbdf7a39845ca20909",
+    "url": "/website/static/media/logo_Kotlin.fb997f5b.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/website/static/media/logo_React.70f87bba.png"
+  },
+  {
+    "revision": "5f31f86e18cb9a35d567f4d2e05883c5",
+    "url": "/website/static/media/nfz_1.5f31f86e.png"
+  },
+  {
+    "revision": "6af5dd46f792e4d51b5b7e547b6fb03a",
+    "url": "/website/static/media/nfz_2.6af5dd46.png"
+  },
+  {
+    "revision": "da1491529b1d16b5240e1becf96d4fea",
+    "url": "/website/static/media/pct2075_1.da149152.png"
+  },
+  {
+    "revision": "90d38fa81d7eab83351a15da18e35809",
+    "url": "/website/static/media/pct2075_2.90d38fa8.png"
+  },
+  {
+    "revision": "dbcebc673d0224de3acc385612ffb8fe",
+    "url": "/website/static/media/photo_me.dbcebc67.jpg"
+  },
+  {
+    "revision": "2ed858a631c1df71834568401fb0acb2",
+    "url": "/website/static/media/pyttleships_1.2ed858a6.png"
+  },
+  {
+    "revision": "750b0d2fe35917a7b628828820dffbb6",
+    "url": "/website/static/media/pyttleships_2.750b0d2f.png"
+  },
+  {
+    "revision": "5ec47ccb47ce41eaaf403cf766aec502",
+    "url": "/website/static/media/spectro_1.5ec47ccb.png"
+  },
+  {
+    "revision": "27946bafa5b4a25b39185f6900e71bcd",
+    "url": "/website/static/media/vending_1.27946baf.png"
+  },
+  {
+    "revision": "3d21f03c8164f40daf2b83961a81df33",
+    "url": "/website/static/media/vending_2.3d21f03c.png"
+  }
+]);
